fa15f0297a01e5d20efce10b05887ec401f4e405
manu <elemaistre@knowmore.fr>
2020-01-20 20:02:44 +0100

    Creation of the project

---
+++ IotProject/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace IotProject.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
