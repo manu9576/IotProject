ea2cd39665fe35849db44c3f5dfdd29bd4ef8843
manu <elemaistre@knowmore.fr>
2020-01-20 20:11:46 +0100

    Add time displaying

--- IotProject/ViewModels/MainWindowViewModel.cs
+++ IotProject/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ReactiveUI;
+using System;
+using System.Reactive;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace IotProject.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel()
+        {
+            Task.Run(() => UpdateValues());
+        }
+
+        private void UpdateValues()
+        {
+            while (true)
+            {
+
+                TimeOfDay = DateTime.Now.ToString("dd/MM/yy HH:mm:ss");
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private string _timeOfDay;
+
+        public string TimeOfDay
+        {
+            get => _timeOfDay;
+            set => this.RaiseAndSetIfChanged(ref _timeOfDay, value);
+        }
     }
 }
