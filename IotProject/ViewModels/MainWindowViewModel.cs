5a9df5bdec1953c3e450f1a02fd39bb00f840349
manu <elemaistre@knowmore.fr>
2020-01-24 21:17:22 +0100

    Creation of SensorViewModel

--- IotProject/ViewModels/MainWindowViewModel.cs
+++ IotProject/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,9 @@
 using Iot.Device.GrovePiDevice.Models;
 using Iot.Device.GrovePiDevice.Sensors;
 using ReactiveUI;
+using Sensors.GrovePi;
 using System;
+using System.Collections.Generic;
 using System.Device.I2c;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,12 +15,11 @@ namespace IotProject.ViewModels
     {
         public MainWindowViewModel()
         {
-
-            I2cConnectionSettings i2CConnectionSettings = new I2cConnectionSettings(1, GrovePi.DefaultI2cAddress);
-            _grovePi = new GrovePi(I2cDevice.Create(i2CConnectionSettings));
-
-            _dhtSensor = new DhtSensor(_grovePi, GrovePort.DigitalPin7, DhtType.Dht11);
-
+            SensorsViewModel = new List<SensorViewModel>
+            {
+                new SensorViewModel(SensorType.DhtTemperatureSensor, GrovePort.DigitalPin7, "Température"),
+                new SensorViewModel(SensorType.DhtHumiditySensor, GrovePort.DigitalPin7, "Humidité")
+            };
             Task.Run(() => UpdateValues());
         }
 
@@ -26,18 +27,23 @@ namespace IotProject.ViewModels
         {
             while (true)
             {
-
                 TimeOfDay = DateTime.Now.ToString("dd/MM/yy HH:mm:ss");
-                _dhtSensor.Read();
-                Sensors = $"Température = {_dhtSensor.LastTemperature} °C (Humidité = {_dhtSensor.LastRelativeHumidity} %)";
+                foreach (var vm in SensorsViewModel)
+                {
+                    vm.Refresh();
+                }
                 Thread.Sleep(1000);
             }
         }
 
         private string _timeOfDay;
-        private string _sensors;
-        private GrovePi _grovePi;
-        private DhtSensor _dhtSensor;
+
+        public List<SensorViewModel> SensorsViewModel
+        {
+            get;
+            private set;
+        }
+
 
         public string TimeOfDay
         {
@@ -45,10 +51,5 @@ namespace IotProject.ViewModels
             set => this.RaiseAndSetIfChanged(ref _timeOfDay, value);
         }
 
-        public string Sensors
-        {
-            get => _sensors;
-            set => this.RaiseAndSetIfChanged(ref _sensors, value);
-        }
     }
 }
---
+++ IotProject/ViewModels/SensorViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Iot.Device.GrovePiDevice.Models;
+using ReactiveUI;
+using Sensors.GrovePi;
+
+namespace IotProject.ViewModels
+{
+    public class SensorViewModel : ViewModelBase
+    {
+        private readonly IGrovePiSensor sensor;
+        private string text;
+
+        public SensorViewModel(IGrovePiSensor sensor)
+        {
+            this.sensor = sensor;
+        }
+
+        public SensorViewModel(SensorType sensorType, GrovePort port, string name)
+        {
+            this.sensor = GrovePiSensorBuilder.CreateSensor(sensorType, port, name);
+        }
+
+        public string Text
+        {
+            get => text;
+            set => this.RaiseAndSetIfChanged(ref text, value);
+        }
+
+        public void Refresh()
+        {
+            Text = sensor.Name + ": " + sensor.Value + " " + sensor.Unit;
+        }
+    }
+}
