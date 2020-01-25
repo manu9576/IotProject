64cd14c5e4ef29559fe8dbf7075bf6cb9726a39a
manu <elemaistre@knowmore.fr>
2020-01-25 15:49:05 +0100

    Creation of fake sensor for debug

---
+++ Sensors/GrovePi/GrovePiFakeSensor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sensors.GrovePi
+{
+    public class GrovePiFakeSensor : IGrovePiSensor
+    {
+        private readonly SensorType sensorType;
+        private readonly Random rand;
+        private readonly double maxValue;
+
+        public GrovePiFakeSensor(SensorType sensorType, string name)
+        {
+            this.sensorType = sensorType;
+            Name = name;
+            rand = new Random();
+
+            switch (sensorType)
+            {
+                case SensorType.AnalogSensor:
+                    maxValue = 5.0;
+                    break;
+
+                case SensorType.GrooveTemperartureSensor:
+                case SensorType.DhtTemperatureSensor:
+                    maxValue = 40.0;
+                    break;
+
+                case SensorType.PotentiometerSensor:
+                case SensorType.DhtHumiditySensor:
+                    maxValue = 100.0;
+                    break;
+
+                case SensorType.UltrasonicSensor:
+                    maxValue = 50.0;
+                    break;
+
+                case SensorType.LightSensor:
+                case SensorType.SoundSensor:
+                default:
+                    throw new Exception("Sensor type '" + sensorType + "' is not implemented");
+            }
+        }
+
+        public string Name { get; private set; }
+
+        public double Value
+        {
+            get
+            {
+                return rand.NextDouble() * maxValue;
+            }
+        }
+
+        public string Unit
+        {
+            get
+            {
+                switch (sensorType)
+                {
+                    case SensorType.AnalogSensor:
+                        return "V";
+
+                    case SensorType.GrooveTemperartureSensor:
+                    case SensorType.DhtTemperatureSensor:
+                        return "°C";
+
+                    case SensorType.PotentiometerSensor:
+                    case SensorType.DhtHumiditySensor:
+                        return "%";
+
+                    case SensorType.UltrasonicSensor:
+                        return "cm";
+
+                    case SensorType.LightSensor:
+                    case SensorType.SoundSensor:
+                    default:
+                        throw new Exception("Sensor type '" + sensorType + "' is not implemented");   
+                }
+            }
+        }
+    }
+}
--- Sensors/GrovePi/GrovePiSensorBuilder.cs
+++ Sensors/GrovePi/GrovePiSensorBuilder.cs
@@ -11,38 +11,42 @@ namespace Sensors.GrovePi
     {
         private static readonly List<GrovePort> _usedPort = new List<GrovePort>();
         private static readonly Dictionary<GrovePort, DhtSensor> _dhtSensor = new Dictionary<GrovePort, DhtSensor>();
-        private static readonly Iot.Device.GrovePiDevice.GrovePi _grovePi = GrovePiDevice.GetDevice();
 
         public static IGrovePiSensor CreateSensor(SensorType sensorType, GrovePort port, string name)
-
         {
+#if DEBUG
+            return new GrovePiFakeSensor(sensorType, name);
+#endif
+
+            var grovePi = GrovePiDevice.GetDevice();
+
             switch (sensorType)
             {
                 case SensorType.AnalogSensor:
                     ThrowExceptionIfPortIsUsed(port);
                     _usedPort.Add(port);
-                    return new GrovePiAnalogSensor(new AnalogSensor(_grovePi, port), name);
+                    return new GrovePiAnalogSensor(new AnalogSensor(grovePi, port), name);
 
                 case SensorType.DhtTemperatureSensor:
-                    return new GrovePiDthTemperatureSensor(GetDhtSensor(port), name);
+                    return new GrovePiDthTemperatureSensor(GetDhtSensor(grovePi, port), name);
 
                 case SensorType.DhtHumiditySensor:
-                    return new GrovePiDthHumiditySensor(GetDhtSensor(port), name);
+                    return new GrovePiDthHumiditySensor(GetDhtSensor(grovePi, port), name);
 
                 case SensorType.PotentiometerSensor:
                     ThrowExceptionIfPortIsUsed(port);
                     _usedPort.Add(port);
-                    return new GrovePiAnalogPotentiometer(new PotentiometerSensor(_grovePi, port), name);
+                    return new GrovePiAnalogPotentiometer(new PotentiometerSensor(grovePi, port), name);
 
                 case SensorType.UltrasonicSensor:
                     ThrowExceptionIfPortIsUsed(port);
                     _usedPort.Add(port);
-                    return new GrovePiAnalogUltrasonic(new UltrasonicSensor(_grovePi, port), name);
+                    return new GrovePiAnalogUltrasonic(new UltrasonicSensor(grovePi, port), name);
 
                 case SensorType.GrooveTemperartureSensor:
                     ThrowExceptionIfPortIsUsed(port);
                     _usedPort.Add(port);
-                    return new GrovePiAnalogTemperature(new GroveTemperatureSensor(_grovePi, port), name);
+                    return new GrovePiAnalogTemperature(new GroveTemperatureSensor(grovePi, port), name);
 
                 case SensorType.LightSensor:
                 case SensorType.SoundSensor:
@@ -59,7 +63,7 @@ namespace Sensors.GrovePi
             }
         }
 
-        private static DhtSensor GetDhtSensor(GrovePort port)
+        private static DhtSensor GetDhtSensor(Iot.Device.GrovePiDevice.GrovePi grovePi, GrovePort port)
         {
             if (_dhtSensor.Keys.Contains(port))
             {
@@ -70,7 +74,7 @@ namespace Sensors.GrovePi
 
             _usedPort.Add(port);
 
-            return new DhtSensor(_grovePi, port, DhtType.Dht11);
+            return new DhtSensor(grovePi, port, DhtType.Dht11);
         }
 
     }
