56d6df242af4fcd5dd9c0d8c9faa52f6523f6b6e
Emmanuel Lemaistre <manu9576@hotmail.fr>
2020-07-21 18:10:15 +0200

    Moving translation functions into new file

--- WebSite/js/ChartHelper.js
+++ WebSite/js/ChartHelper.js
@@ -1,118 +1,10 @@
-let translate_month = function (month) {
-
-    var result = month;
-
-    switch (month) {
-        case 'Jan':
-            result = 'Janv';
-            break;
-        case 'Feb':
-            result = 'Fév';
-            break;
-        case 'Mar':
-            result = 'Mars';
-            break;
-        case 'Apr':
-            result = 'Avr';
-            break;
-        case 'May':
-            result = 'Mai';
-            break;
-        case 'Jun':
-            result = "Juin"
-            break;
-        case 'Jul':
-            result = "Juil"
-            break;
-        case 'Aug':
-            result = 'Août';
-            break;
-        case 'Sep':
-            result = 'Sept';
-            break;
-        case 'Oct':
-            result = 'Oct';
-            break;
-        case 'Nov':
-            result = 'Nov';
-            break;
-        case 'Dec':
-            result = 'Déc';
-            break;
-    }
-
-    return result;
-};
-
-let translate_day = function (day) {
-
-    var result = day;
-
-    switch (day) {
-        case 'Monday':
-            result = 'Lun';
-            break;
-        case 'Tuesday':
-            result = 'Mar';
-            break;
-        case 'Wednesday':
-            result = 'Mer';
-            break;
-        case 'Thursday':
-            result = 'Jeu';
-            break;
-        case 'Friday':
-            result = 'Ven';
-            break;
-        case 'Saturday':
-            result = "Sam"
-            break;
-        case 'Sunday':
-            result = "Dim"
-            break;
-    }
-
-    return result;
-};
-
-
-let translate_date_label = function (label) {
-
-    if(!isFrenchLanguage()){
-        return label;
-    }
-
-    let month = label.match(/Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec/g);
-
-    let day = label.match(/Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday/g);
-
-    let translatedDate = label;
-
-    if (month) {
-        let translation = translate_month(month[0]);
-        translatedDate = translatedDate.replace(month, translation);
-    }
-
-    if (day) {
-        let translation = translate_day(day[0]);
-        translatedDate = translatedDate.replace(day, translation);
-    }
-
-    return translatedDate;
-};
-
-let isFrenchLanguage= function(){
-    return navigator.language = "fr" || navigator.language == 'fr-FR';
-}
-
 class ChartHelper {
 
     constructor(chart) {
 
-        if(isFrenchLanguage()){
-            moment.locale('fr');
-        }
+        let self = this;
 
+        this.translator = new Translator();
         this.datasets = [];
         this.ctx = chart.getContext('2d');
         this.tooltips = {
@@ -162,7 +54,7 @@ class ChartHelper {
             },
             ticks: {
                 callback: function (label, index, labels) {
-                    return translate_date_label(label);
+                    return self.translator.translate_date_label(label);
                 }
             }
         }];
@@ -186,6 +78,10 @@ class ChartHelper {
 
         }];
 
+        if(this.translator.isFrenchLanguage()){
+            moment.locale('fr');
+        }
+
         this.updateChart();
     }
 
@@ -199,6 +95,10 @@ class ChartHelper {
 
     updateChart() {
 
+        if(this.chart){
+            this.chart.destroy();
+        }
+
         this.chart = new Chart(this.ctx, {
             type: 'line',
             data: {
@@ -207,13 +107,16 @@ class ChartHelper {
             options: {
                 responsive: true,
                 maintainAspectRatio: false,
+                responsiveAnimationDuration: 0,
+
                 animation: {
                     duration: 0
                 },
+
                 hover: {
                     animationDuration: 0
                 },
-                responsiveAnimationDuration: 0,
+                
                 legend: {
                     display: false
                 },
---
+++ WebSite/js/Translator.js
@@ -0,0 +1,110 @@
+
+class Translator {
+
+    _translate_month(month) {
+
+        var result = month;
+
+        switch (month) {
+            case 'Jan':
+                result = 'Janv';
+                break;
+            case 'Feb':
+                result = 'Fév';
+                break;
+            case 'Mar':
+                result = 'Mars';
+                break;
+            case 'Apr':
+                result = 'Avr';
+                break;
+            case 'May':
+                result = 'Mai';
+                break;
+            case 'Jun':
+                result = "Juin"
+                break;
+            case 'Jul':
+                result = "Juil"
+                break;
+            case 'Aug':
+                result = 'Août';
+                break;
+            case 'Sep':
+                result = 'Sept';
+                break;
+            case 'Oct':
+                result = 'Oct';
+                break;
+            case 'Nov':
+                result = 'Nov';
+                break;
+            case 'Dec':
+                result = 'Déc';
+                break;
+        }
+
+        return result;
+    };
+
+    _translate_day(day) {
+
+        var result = day;
+
+        switch (day) {
+            case 'Monday':
+                result = 'Lun';
+                break;
+            case 'Tuesday':
+                result = 'Mar';
+                break;
+            case 'Wednesday':
+                result = 'Mer';
+                break;
+            case 'Thursday':
+                result = 'Jeu';
+                break;
+            case 'Friday':
+                result = 'Ven';
+                break;
+            case 'Saturday':
+                result = "Sam"
+                break;
+            case 'Sunday':
+                result = "Dim"
+                break;
+        }
+
+        return result;
+    };
+
+    translate_date_label(label) {
+
+        if (!this.isFrenchLanguage()) {
+            return label;
+        }
+
+        let month = label.match(/Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec/g);
+
+        let day = label.match(/Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday/g);
+
+        let translatedDate = label;
+
+        if (month) {
+            let translation = this._translate_month(month[0]);
+            translatedDate = translatedDate.replace(month, translation);
+        }
+
+        if (day) {
+            let translation = this._translate_day(day[0]);
+            translatedDate = translatedDate.replace(day, translation);
+        }
+
+        return translatedDate;
+    };
+
+    isFrenchLanguage() {
+        return navigator.language == "fr" || navigator.language == 'fr-FR';
+    }
+
+}
