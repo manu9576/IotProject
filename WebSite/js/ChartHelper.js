5b5226999b45fbeafa8f05ee5ef5e4e869578100
Emmanuel Lemaistre <manu9576@hotmail.fr>
2020-07-21 13:16:59 +0200

    Adding localisation for date in chart

--- WebSite/js/ChartHelper.js
+++ WebSite/js/ChartHelper.js
@@ -1,9 +1,107 @@
-class ChartHelper {
+let translate_month = function (month) {
 
+    var result = month;
+
+    switch (month) {
+        case 'Jan':
+            result = 'Janv';
+            break;
+        case 'Feb':
+            result = 'Fév';
+            break;
+        case 'Mar':
+            result = 'Mars';
+            break;
+        case 'Apr':
+            result = 'Avr';
+            break;
+        case 'May':
+            result = 'Mai';
+            break;
+        case 'Jun':
+            result = "Juin"
+            break;
+        case 'Jul':
+            result = "Juil"
+            break;
+        case 'Aug':
+            result = 'Août';
+            break;
+        case 'Sep':
+            result = 'Sept';
+            break;
+        case 'Oct':
+            result = 'Oct';
+            break;
+        case 'Nov':
+            result = 'Nov';
+            break;
+        case 'Dec':
+            result = 'Déc';
+            break;
+    }
+
+    return result;
+};
+
+let translate_day = function (day) {
+
+    var result = day;
+
+    switch (day) {
+        case 'Monday':
+            result = 'Lun';
+            break;
+        case 'Tuesday':
+            result = 'Mar';
+            break;
+        case 'Wednesday':
+            result = 'Mer';
+            break;
+        case 'Thursday':
+            result = 'Jeu';
+            break;
+        case 'Friday':
+            result = 'Ven';
+            break;
+        case 'Saturday':
+            result = "Sam"
+            break;
+        case 'Sunday':
+            result = "Dim"
+            break;
+    }
+
+    return result;
+};
+
+
+let translate_this_label = function (label) {
+
+    let month = label.match(/Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec/g);
+
+    let day = label.match(/Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday/g);
+
+    let translatedDate = label;
+
+    if (month) {
+        let translation = translate_month(month[0]);
+        translatedDate = translatedDate.replace(month, translation);
+    }
+
+    if (day) {
+        let translation = translate_day(day[0]);
+        translatedDate = translatedDate.replace(day, translation);
+    }
+
+    return translatedDate;
+};
+
+class ChartHelper {
 
     constructor(chart) {
 
-
+        moment.locale('fr');
         this.datasets = [];
         this.ctx = chart.getContext('2d');
 
@@ -26,10 +124,22 @@ class ChartHelper {
                         distribution: 'linear',
                         time: {
                             unit: 'day',
+                            tooltipFormat: 'dddd DD MMM, YYYY',
                             unitStepSize: 1,
                             displayFormats: {
-                                'day': 'dddd DD MMM'
-                            },
+                                day: 'dddd DD MMM'
+                            }
+                        },
+                        scaleLabel: {
+                            display: true,
+                        },
+                        ticks: {
+
+                            // Here's where the magic happens:
+                            callback: function (label, index, labels) {
+
+                                return translate_this_label(label);
+                            }
                         }
                     }],
                     yAxes: [{
@@ -58,6 +168,8 @@ class ChartHelper {
         this.yAxes = this.chart.options.scales.yAxes;
         this.xAxes = this.chart.options.scales.xAxes;
     }
+
+
 
     clearDatasets() {
         this.chart.data.datasets.length = 0;
@@ -89,7 +201,5 @@ class ChartHelper {
 
             }
         });
-
     }
-
 }
